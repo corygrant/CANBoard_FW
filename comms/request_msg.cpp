de5dbc89cf62f206fd024819c6b247d9dcde8bc2
Cory Grant <cory.grant11@gmail.com>
2026-04-19 23:37:02 -0400

    add burn request back

--- comms/request_msg.cpp
+++ comms/request_msg.cpp
@@ -7,15 +7,37 @@
 void CheckRequestMsgs(CANRxFrame *frame)
 {
     //Check for settings request message
-    if(frame->SID != 0x080)
+    if(frame->SID != stConfig.stDevConfig.nParamRxId)
         return;
+
+    // Check for burn request
+    if ((frame->DLC == 8) && 
+        (frame->data8[0] == static_cast<uint8_t>(MsgCmd::BurnSettings)) &&
+        (frame->data8[1] == 1) &&
+        (frame->data8[2] == 3) && 
+        (frame->data8[3] == 8))
+    {
+        CANTxFrame txMsg;
+        txMsg.SID = stConfig.stDevConfig.nParamTxId;
+        txMsg.IDE = CAN_IDE_STD;
+        txMsg.DLC = 8;
+        txMsg.data8[0] = static_cast<uint8_t>(MsgCmd::BurnSettings);
+        txMsg.data8[1] = 1;
+        txMsg.data8[2] = 3;
+        txMsg.data8[3] = 8;
+        txMsg.data8[4] = WriteConfig();
+        txMsg.data8[5] = 0;
+        txMsg.data8[6] = 0;
+        txMsg.data8[7] = 0;
+        PostTxFrame(&txMsg);
+    }
 
     // Check for version request
     if ((frame->DLC == 8) &&
         (frame->data8[0] == static_cast<uint8_t>(MsgCmd::Version)))
     {
         CANTxFrame txMsg;
-        txMsg.SID = 0x081;
+        txMsg.SID = stConfig.stDevConfig.nParamTxId;
         txMsg.IDE = CAN_IDE_STD;
         txMsg.DLC = 8;
         txMsg.data8[0] = static_cast<uint8_t>(MsgCmd::Version);
